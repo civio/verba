035458ad4035d88b2094ebcc70641d27fbcbd86f
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-21 19:27:12 +0100

    Add basic Results component

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,15 +2,18 @@
   <div id="app" class="container text-center">
     <h1 class="my-4">Verba</h1>
     <Search/>
+    <Results/>
   </div>
 </template>
 
 <script>
+import Results from './components/Results.vue'
 import Search from './components/Search.vue'
 
 export default {
   name: 'app',
   components: {
+    Results,
     Search
   }
 }
---
+++ web/src/components/Results.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="my-4">
+    <ul class="list-group">
+      <li class="list-group-item" v-for="item in results" :key="item.id">
+        {{ item.programme.title }}
+        <br>
+        <small>({{ item.time_start }} - {{ item.time_end }})</small>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Results',
+  computed: mapState({
+    results: state => state.results
+  })
+}
+</script>
