8108595d421ba7caa457272978046d76aa4b0d98
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-21 19:25:29 +0100

    Add cors support in api

--- api/src/index.js
+++ api/src/index.js
@@ -1,19 +1,22 @@
+import cors from 'cors'
 import express from 'express'
 import Captions from './captions'
+
+const PORT = process.env.PORT || 8888
 
 // Create captions model
 const captions = new Captions('http://api-verba-volant.civio.es')
 
-// Create express app & router instance
+// Create express app
 const app = express()
-const router = express.Router()
-const PORT = process.env.PORT || 8888
 
-router.get('/', (request, response) => {
+app.use(cors())
+
+app.get('/', (request, response) => {
   response.json({ name: 'Verba Volant API', version: '1.0' })
 })
 
-router.get('/search', async (request, response) => {
+app.get('/search', cors(), async (request, response) => {
   if (request.query.q) {
     const results = await captions.search(
       request.query.q,
@@ -27,5 +30,4 @@ router.get('/search', async (request, response) => {
 })
 
 // Register express routes & serve
-app.use('/api', router)
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
