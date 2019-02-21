7565d7031fc44edd344e4c204d5a16b659cf7b4d
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-21 19:26:31 +0100

    Setup store with results state & search action & mutation

--- web/src/store.js
+++ web/src/store.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+
 Vue.use(Vuex)
+
+const API_URL = 'http://localhost:8888/'
 
 export default new Vuex.Store({
   state: {
-
-  },
-  mutations: {
-
+    results: []
   },
   actions: {
-
+    search(context, payload) {
+      context.commit('search', payload)
+    }
+  },
+  mutations: {
+    search(state, payload) {
+      axios
+        .get(API_URL + 'search', {
+          params: {
+            q: payload
+          }
+        })
+        .then(response => {
+          state.results = response.data
+          console.log(state.results)
+        })
+    }
   }
 })
