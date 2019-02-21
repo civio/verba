189154b30ae8167438cc5a69b71124f4dd0977c4
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-21 18:00:27 +0100

    Add basic frontend app
    Basic Vue app

---
+++ web/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Verba Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
