47a95d699328fc31ae56c164c2fcbed1aa5fed27
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-22 17:31:22 +0100

    Add SearchFilters component

---
+++ web/src/components/SearchFilters.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="search-filters">
+    <button class="btn btn-sm btn-outline-secondary">Date</button>
+    <button class="btn btn-sm btn-outline-secondary">Type</button>
+    <button class="btn btn-sm btn-outline-secondary">Group</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchFilters"
+};
+</script>
+
+<style lang="scss" scoped>
+.search-filters {
+  margin-top: 1.25rem;
+}
+.btn {
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+</style>
