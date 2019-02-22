dcfdca238e7d200b240f0168a295b9687a95d5fd
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-22 18:40:56 +0100

    Add date picker to SearchFilters component

--- web/src/components/SearchFilters.vue
+++ web/src/components/SearchFilters.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="search-filters">
-    <button class="btn btn-sm btn-outline-secondary">Date</button>
+    <button class="btn btn-sm btn-outline-secondary" @click="onDateClick">Date</button>
     <button class="btn btn-sm btn-outline-secondary">Type</button>
     <button class="btn btn-sm btn-outline-secondary">Group</button>
+    <div class="datepicker-container" v-show="datepickerVisible">
+      <div id="datepicker"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import Pikaday from "pikaday";
+
 export default {
-  name: "SearchFilters"
+  name: "SearchFilters",
+  data: function() {
+    return {
+      datepickerVisible: false
+    };
+  },
+  mounted: function() {
+    // add pikaday datepicker
+    var field = document.getElementById("datepicker");
+    this.datepicker = new Pikaday({
+      onSelect: date => {
+        console.log(this.datepicker.toString());
+      }
+    });
+    field.parentNode.insertBefore(this.datepicker.el, field.nextSibling);
+  },
+  methods: {
+    onDateClick() {
+      this.datepickerVisible = !this.datepickerVisible;
+    }
+  }
 };
 </script>
 
