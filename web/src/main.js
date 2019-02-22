fa6b7ada9216f1f467ec8da475b7c64e2803499f
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-22 17:29:56 +0100

    Add formatDate & formatTime Vue fiters

--- web/src/main.js
+++ web/src/main.js
@@ -4,6 +4,21 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.filter('formatDate', str => {
+  const date = new Date(str)
+  return `${date.toLocaleDateString('es-ES', {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit'
+  })} - TD ${date.getUTCHours()}h`
+})
+
+Vue.filter('formatTime', time => {
+  const min = Math.floor(time / 60)
+  const sec = time % 60
+  return `${min}'${sec < 10 ? '0' + sec : sec}''`
+})
+
 new Vue({
   store,
   render: h => h(App)
