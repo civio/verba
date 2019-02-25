7835f4e2dbdcac863214aecd2e7768429382b8e1
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-25 18:20:43 +0100

    Improve Datepicker component
    - Set pickers maxDate as today
    - Clear pickers dates on reset
    - Set date_to as date_from if not defined
    - Improve styles

--- web/src/components/Datepicker.vue
+++ web/src/components/Datepicker.vue
@@ -33,21 +33,34 @@ export default {
   mounted: function() {
     // setup start & end Pikaday pickers
     // TODO: disable future dates
-    this.startPicker = new Pikaday()
-    this.endPicker = new Pikaday()
+    const config = {
+      maxDate: new Date()
+    }
+    this.startPicker = new Pikaday(config)
+    this.endPicker = new Pikaday(config)
     // add pikaday datepickers to datepicker-container
     document.getElementById('start-picker').appendChild(this.startPicker.el)
     document.getElementById('end-picker').appendChild(this.endPicker.el)
   },
   methods: {
     onClickApply() {
+      const from = this.startPicker.getDate()
+      const to = this.endPicker.getDate()
       // fire change event with from & to dates
-      this.$emit('change', {
-        from: this.startPicker.getDate(),
-        to: this.endPicker.getDate()
-      })
+      this.$emit(
+        'change',
+        from
+          ? {
+              from: from,
+              to: to ? to : from
+            }
+          : null
+      )
     },
     onClickReset() {
+      // clear pickers
+      this.startPicker.setDate(null)
+      this.endPicker.setDate(null)
       // fire emit event with null date
       this.$emit('change', null)
     }
@@ -59,8 +72,53 @@ export default {
 .datepicker-container {
   border: 1px solid #6c757d;
   border-radius: 0.2rem; // btn border radius
+
   .pika-single {
     border: none;
+  }
+  .pika-table {
+    th {
+      font-weight: 400;
+    }
+    td {
+      border: 1px solid #ddd;
+      text-align: center;
+
+      &.is-empty {
+        border: none;
+      }
+    }
+    abbr {
+      text-decoration: none;
+      cursor: default;
+    }
+  }
+  .pika-button {
+    color: #212529;
+    padding: 7px 4px 5px;
+    text-align: center;
+    background: none;
+    border-radius: 0;
+
+    &:hover {
+      color: #212529;
+      background: #ddd;
+      border-radius: 0;
+    }
+  }
+  .is-selected .pika-button,
+  .has-event .pika-button {
+    font-weight: 400;
+    background: #007bff; // brand-primary
+    border-radius: 0;
+    box-shadow: none;
+  }
+  .is-disabled .pika-button {
+    color: #bbb;
+  }
+  .is-disabled .pika-button,
+  .is-inrange .pika-button {
+    background: none;
   }
 }
 
