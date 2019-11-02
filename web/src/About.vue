65989c9912708e80cd19950cd656a86e70875d67
David Cabo <david.cabo@gmail.com>
2019-11-02 23:06:12 +0100

    Document basic Elastic operators #31

--- web/src/About.vue
+++ web/src/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    About
+    <h4>About</h4>
+
+    <p>Full documentation available at <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-simple-query-string-query.html#simple-query-string-syntax" target="_blank">Elastic site</a>. Most important operators:</p>
+
+    <dl class="row">
+      <dt class="col-sm-1">+</dt>
+      <dd class="col-sm-11">signifies AND operation</dd>
+
+      <dt class="col-sm-1">|</dt>
+      <dd class="col-sm-11">signifies OR operation</dd>
+
+      <dt class="col-sm-1">-</dt>
+      <dd class="col-sm-11">negates a single token</dd>
+
+      <dt class="col-sm-1">"</dt>
+      <dd class="col-sm-11">wraps a number of tokens to signify a phrase for searching</dd>
+
+      <dt class="col-sm-1">*</dt>
+      <dd class="col-sm-11">at the end of a term signifies a prefix query</dd>
+    </dl>
+
+    <p>By default, AND is applied to search terms, so "Obama Trump" will return results with BOTH "Obama" and "Trump" in them. If you want to find results with EITHER keyword, use "Obama | Trump"</p>
   </div>
 </template>
