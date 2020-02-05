03bdade96a3306f4921f52058c0725193634b76e
carmen-tm <carmentorremolina@gmail.com>
2020-02-05 16:34:58 +0100

    Fix links styles for civio/verba#52

--- web/src/components/Pagination.vue
+++ web/src/components/Pagination.vue
@@ -1,27 +1,19 @@
 <template>
   <nav v-show="size > 1">
     <ul class="pagination justify-content-center">
-      <li
-        :class="{ disabled: current === 0, 'page-item': true }"
-        @click="onClickPrev"
-      >
+      <li :class="{ disabled: current === 0, 'page-item': true }" @click="onClickPrev">
         <a class="page-link" href="#" :aria-label="$t('pagination.previous')">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
-      <li
-        v-for="i in pages"
-        :key="i"
-        :class="{ active: i === current + 1, 'page-item': true }"
-      >
-        <a class="page-link" :data-index="i - 1" @click="onClickPage">{{
+      <li v-for="i in pages" :key="i" :class="{ active: i === current + 1, 'page-item': true }">
+        <a class="page-link" :data-index="i - 1" @click="onClickPage">
+          {{
           i
-        }}</a>
+          }}
+        </a>
       </li>
-      <li
-        :class="{ disabled: current === size - 1, 'page-item': true }"
-        @click="onClickNext"
-      >
+      <li :class="{ disabled: current === size - 1, 'page-item': true }" @click="onClickNext">
         <a class="page-link" href="#" :aria-label="$t('pagination.previous')">
           <span aria-hidden="true">&raquo;</span>
         </a>
@@ -79,6 +71,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../scss/_variables.scss';
+
 .page-item {
   cursor: pointer;
 }
@@ -88,5 +82,10 @@ export default {
 }
 .page-link {
   font-size: 0.875rem;
+  color: $color-neutral-1000;
+}
+.page-item.active .page-link {
+  background-color: $color-neutral-800;
+  border-color: $color-neutral-800;
 }
 </style>
