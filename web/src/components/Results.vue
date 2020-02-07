33cc7d3388e4c8036433a2c2fc6dd65397a41846
David Cabo <david.cabo@gmail.com>
2020-02-07 13:26:13 +0100

    Fix spacing in result count

--- web/src/components/Results.vue
+++ web/src/components/Results.vue
@@ -16,11 +16,11 @@
             v-if="results.length > 50"
             class="text-secondary"
           >{{ $t('results.list.page', { p: resultsPage + 1, total: Math.ceil(results.length / 50) }) }}</span>
+          &nbsp;
           <span
             v-html="$t('results.list.count', { total: results.length.toLocaleString(), query: query })"
           ></span>
-          &mdash; (
-          <a href="#" @click="onDownloadClick()">CSV</a>)
+          &mdash; (<a href="#" @click="onDownloadClick()">CSV</a>)
         </p>
         <div class="results-list mb-4">
           <div v-for="(items, id) in resultsByProgramme" :key="id" class="card">
