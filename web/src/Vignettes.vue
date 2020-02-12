f0b5a870da216268413624952e8ae4b1ebd9ae75
carmen-tm <carmentorremolina@gmail.com>
2020-02-12 11:28:25 +0100

    Add copies for civio/verba#58

--- web/src/Vignettes.vue
+++ web/src/Vignettes.vue
@@ -28,8 +28,12 @@
             <h4 class="methodology-title" v-html="$t('vignettes.methodology')"></h4>
             <div class="query-container">
               <p class="methodology" v-html="$t('vignettes.query-subtitle')"></p>
-              <div class="query-container">
-                <p class="methodology-query">"ultraderecha"|"ultra derecha"</p>
+              <div>
+                <p class="methodology-query">"ultraderecha"|"ultra derecha"</p>&nbsp;
+                <a
+                  href="https://verba.civio.es/?q=%22ultraderecha%22%20%7C%20%22ultra%20derecha%22#verba-subtitle-dataviz"
+                  target="_blank"
+                >(link)</a>
               </div>
             </div>
             <p class="methodology-note"></p>
@@ -41,13 +45,17 @@
           <h3 class="vignette-title">Gürtel y los ERE, casi empatados</h3>
           <div class="vignette-copy">
             <p>
-              La <mark>corrupción</mark> es, por desgracia, una constante en los informativos de TVE. La palabra ha aparecido más de 4.000 veces desde 2014 y hasta el 31 de enero de 2020 como un goteo constante, aunque algo menos intenso en los últimos años. De hecho, 2.428 veces lo hizo entre 2014 y 2016; y 1.554, ya con menos ritmo, en los tres años siguientes.
-              </p>
+              La
+              <mark>corrupción</mark> es, por desgracia, una constante en los informativos de TVE. La palabra ha aparecido más de 4.000 veces desde 2014 y hasta el 31 de enero de 2020 como un goteo constante, aunque algo menos intenso en los últimos años. De hecho, 2.428 veces lo hizo entre 2014 y 2016; y 1.554, ya con menos ritmo, en los tres años siguientes.
+            </p>
             <p>
-              El momento álgido se vivió a finales de octubre de 2014, con las detenciones del caso Púnica, que se combinaron con debates parlamentarios y propuestas y pactos en el Congreso para acabar con el problema. Pero la Púnica (357 apariciones) no ha sido la saga corrupta más nombrada. El caso Gürtel (713) y el de los ERE (804) están casi empatados.
+              El momento álgido se vivió a finales de octubre de 2014, con las detenciones del caso Púnica, que se combinaron con debates parlamentarios y propuestas y pactos en el Congreso para acabar con el problema. Pero la Púnica (357 apariciones) no ha sido la saga corrupta más nombrada. El caso
+              <mark
+                class="item1"
+              >Gürtel</mark> (713) y el de los
+              <mark class="item2">ERE</mark> (804) están casi empatados.
             </p>
 
-  
             <div class="chart-annotated">
               <img
                 src="./images/verba-item2-corrupcion_modif01.svg"
@@ -56,12 +64,18 @@
             </div>
 
             <p>
-              Durante la última semana de mayo de 2018, justo después de la sentencia de Gürtel, con la Audiencia Nacional decidiendo si fijaba medidas cautelares para los acusados y la moción de censura contra Mariano Rajoy en marcha, este caso de corrupción fue la estrella de los Telediarios, con su apogeo el día 31, el de la moción que cambió a Rajoy por Sánchez en la presidencia del Gobierno.
+              Durante la última semana de mayo de 2018, justo después de la sentencia de
+              <mark
+                class="item1"
+              >Gürtel</mark>
+              , con la Audiencia Nacional decidiendo si fijaba medidas cautelares para los acusados y la moción de censura contra Mariano Rajoy en marcha, este caso de corrupción fue la estrella de los Telediarios, con su apogeo el día 31, el de la moción que cambió a Rajoy por Sánchez en la presidencia del Gobierno.
             </p>
 
             <p>
-              Para el caso de los ERE, esa fecha mágica llegó el 19 de noviembre, con la sentencia que condenó a los expresidentes andaluces Manuel Chaves y José Antonio Griñán, entre otros.
-            <p/>
+              Para el caso de los
+              <mark class="item2">ERE</mark> , esa fecha mágica llegó el 19 de noviembre, con la sentencia que condenó a los expresidentes andaluces Manuel Chaves y José Antonio Griñán, entre otros.
+            </p>
+            <p />
 
             <div class="chart-annotated">
               <img
@@ -74,21 +88,61 @@
             <h4 class="methodology-title" v-html="$t('vignettes.methodology')"></h4>
             <div class="query-container">
               <p class="methodology" v-html="$t('vignettes.query-subtitle')"></p>
-              <div class="query-container"></div>
+              <div>
+                <p class="methodology-query">"corrupción" | "corrupcion"</p>&nbsp;
+                <a
+                  href="https://verba.civio.es/?q=corrupci%C3%B3n%20%7C%20corrupcion#verba-subtitle-dataviz"
+                  target="_blank"
+                >(link)</a>
+                <br />
+                <p class="methodology-query">"gürtel" | "gurtel"</p>&nbsp;
+                <a
+                  href="https://verba.civio.es/?q=%22g%C3%BCrtel%22%20%7C%20%22gurtel%22#verba-subtitle-dataviz"
+                  target="_blank"
+                >(link)</a>
+                &nbsp;
+                VS
+                &nbsp;
+                <p class="methodology-query">"los ere" | "caso ERE"</p>&nbsp;
+                <a
+                  href="https://verba.civio.es/?q=%22los%20ere%22%20%7C%20%22caso%20ERE%22#verba-subtitle-dataviz"
+                  target="_blank"
+                >(link)</a>
+              </div>
             </div>
-            <p class="methodology-note"></p>
+            <p
+              class="methodology-note"
+            >Nota: A la búsqueda de los ERES hemos eliminado cuatro casos en los que trataban Expedientes de Regulación de Empleo distintos a los del caso de corrupción.</p>
           </div>
         </li>
 
         <!-- Tema 3 - dieta -->
         <li class="verba-vignettes-item">
           <h3 class="vignette-title">El rigor científico, a dieta</h3>
-          <div class="vignette-copy"></div>
+          <div class="vignette-copy">
+            <p>
+              Televisión Española mencionó en sus informativos 380 veces la palabra
+              <mark>"dieta"</mark> entre enero de 2014 y enero de 2020. Pero los informativos, en ocasiones, también han puesto a dieta el rigor científico, sustituyéndolo por un cóctel de desinformación calórica y desequilibrada, que puede poner en riesgo la salud.
+            </p>
+            <p>Uno de los mantras citados por RTVE es que tomar una o dos copas de vino al día es beneficioso. Pero nada más lejos de la realidad: el consumo moderado y saludable de alcohol es un gran mito, que no previene los problemas cardiovasculares y que, de hecho, puede aumentar el riesgo de enfermedades como el cáncer. Este no es el único gazapo de Televisión Española en temas relacionados con la alimentación. La cadena pública también se hizo eco de un trabajo que afirmaba que la pasta no engorda. ¿El problema? El estudio estaba patrocinado por Barilla, un detalle que los informativos olvidaron mencionar en su cobertura.</p>
+            <p>Durante los últimos años, TVE ha difundido informaciones sobre alimentos que pueden inducir a error a los consumidores. Uno de sus telediarios, hablando de la llamada dieta del delfín, llegó a afirmar que "beber agua de mar es una tendencia creciente en el campo de las supuestas dietas saludables". Pese a que la televisión pública menciona la palabra "charlatanería", habla del agua de mar como un "completo nutriente con propiedades". Verba también muestra la cobertura negativa de TVE sobre los aditivos en los alimentos, pese a tratarse de sustancias seguras que ayudan a mejorar el aspecto o textura de los mismos y a conservarlos mejor.</p>
+            <p>Al cubrir la información sobre peligrosas modas, como la de la leche cruda, los informativos de Televisión Española tampoco han destacado el riesgo que supone el consumo de esta bebida. Del total de 11 resultados obtenidos, varios apuntan hacia la producción de quesos utilizando este tipo de leche sin pasteurizar, y otros hacia la polémica normativa aprobada en Cataluña hace casi dos años. Del riesgo que produce su consumo, como la intoxicación por peligrosas bacterias como Listeria, Salmonella o Escherichia coli, ni rastro.</p>
+          </div>
           <div class="vignette-methodology">
             <h4 class="methodology-title" v-html="$t('vignettes.methodology')"></h4>
             <div>
-              <p class="methodology"></p>
-              <div class="query-container"></div>
+              <div class="query-container">
+                <p class="methodology" v-html="$t('vignettes.query-subtitle')"></p>
+                <div>
+                  <p
+                    class="methodology-query"
+                  >"dieta" | "copa de vino" | "pasta engorda" | "leche cruda" | "aditivos"</p>&nbsp;
+                  <a
+                    href="https://verba.civio.es/?q=%22dieta%22%20%7C%20%22copa%20de%20vino%22%20%7C%20%22pasta%20engorda%22%20%7C%20%22leche%20cruda%22%20%7C%20%22aditivos%22#verba-subtitle-dataviz"
+                    target="_blank"
+                  >(link)</a>
+                </div>
+              </div>
             </div>
             <p class="methodology-note"></p>
           </div>
@@ -97,26 +151,69 @@
         <!-- Tema 4 - rajoy -->
         <li class="verba-vignettes-item">
           <h3 class="vignette-title">Rajoy sigue vivo</h3>
-          <div class="vignette-copy"></div>
+          <div class="vignette-copy">
+            <p>
+              ¿Quién es el personaje más nombrado del Telediario? Si dios ha aparecido unas mil veces desde 2014, Messi suma más de seis dioses (6.467) y Trump, más de siete (7.603). Pero hay alguien que dobla al astro argentino y al endiosado presidente estadounidense.
+              <mark>Mariano Rajoy</mark> ha sido mencionado 13.134 veces. Tiene cierta lógica, porque fue presidente durante casi todo el periodo que abarca Verba (de enero de 2014 a junio de 2018). Pero su peso como expresidente sigue siendo considerable.
+            </p>
+            <p>Desde que fue sustituido en su escaño por un bolso mientras pasaba ocho horas en un bar cercano al Congreso de los Diputados, donde se celebraba la moción que acabó con su presidencia, ha sido mencionado 628 veces en los Telediarios de la televisión pública. Parece poco, pero no lo es si tenemos en cuenta que estamos hablando de solo 20 meses, hasta el 31 de enero de 2020. Así, de media, se le menta una vez al día, una cifra muy superior a las de los otros expresidentes.</p>
+            <p>A José María Aznar (503) y Felipe González (469) se les hace casito cada tanto en el Telediario y ambos rondan las 500 apariciones en algo más de seis meses. Zapatero, más cercano en el tiempo (el registro empieza tres años después de su adiós), les supera, con 781.</p>
+            <p>Puigdemont, por su parte, aparece 4.436 veces, con tres hitos: el referendum y la posterior fuga en octubre de 2017, el pleno de investidura en enero de 2018 y la detención en Alemania en marzo de ese mismo año.</p>
+            <p>Puigdemont, por su parte, aparece 4.436 veces, con tres hitos: el referendum y la posterior fuga en octubre de 2017, el pleno de investidura en enero de 2018 y la detención en Alemania en marzo de ese mismo año.</p>
+          </div>
           <div class="vignette-methodology">
             <h4 class="methodology-title" v-html="$t('vignettes.methodology')"></h4>
             <div>
-              <p class="methodology"></p>
-              <div class="query-container"></div>
+              <div class="query-container">
+                <p class="methodology" v-html="$t('vignettes.query-subtitle')"></p>
+                <div>
+                  <p class="methodology-query">"rajoy"</p>&nbsp;
+                  <a
+                    href="https://verba.civio.es/?q=%22dieta%22%20%7C%20%22copa%20de%20vino%22%20%7C%20%22pasta%20engorda%22%20%7C%20%22leche%20cruda%22%20%7C%20%22aditivos%22#verba-subtitle-dataviz"
+                    target="_blank"
+                  >(link)</a>
+                  <br />
+                  <p
+                    class="methodology-query"
+                  >"pedro sanchez" | "pedro sánchez"; "aznar"; "felipe gonzalez" | "felipe gonzález"; "puigdemont"; "trump"; "messi" | "mesi"; "dios"; "zapatero" *</p>
+                </div>
+              </div>
             </div>
-            <p class="methodology-note"></p>
+            <p class="methodology-note">*(puede haber zapateros)</p>
           </div>
         </li>
 
         <!-- Tema 5 - clima -->
         <li class="verba-vignettes-item">
           <h3 class="vignette-title">No había crisis climática hasta 2019</h3>
-          <div class="vignette-copy"></div>
+          <div class="vignette-copy">
+            <p>
+              Cuando a finales del 2019, Greta Thunberg dió un discurso en la Cumbre del Clima de Madrid, dejó claro su enfado y sentenció: "Mientras la crisis climática continúa siendo ignorada por quienes tienen que tomar las decisiones, hay personas muriendo a día de hoy". No sabemos qué ocurre con aquellos que tienen que decidir, pero lo que tenemos claro es que la
+              <mark
+                class="item2"
+              >"crisis climática"</mark>en los telediarios de RTVE no empezó a aparecer hasta 2019. Solo dos veces antes se había citado como tal: en 2016, por la ONG Acción contra el Hambre, y en la boca de Greta en 2018. De la propia Greta se ha hablado desde ese mismo momento, en concreto, en 75 ocasiones.
+            </p>
+            <p>
+              Antes de esta crisis, RTVE ya hablaba de
+              <mark class="item1">cambio climático:</mark> desde 2014 hasta principios de 2020, los informativos han citado estas palabras más de 1.350 veces. Solo en 2019 se llegaron a utilizar en casi 600 ocasiones. Y, aunque la Fundeu recomiende referirise al problema como "crisis climática", en los informativos ha aparecido otro concepto aún más popular: "emergencia climática". Apareció por primera vez en el telediario del 15 de diciembre de 2019 y, hasta el 31 de enero de 2020, se ha utilizado 60 veces. Parece que ha llegado para quedarse, al igual que el problema que refleja.
+            </p>
+          </div>
           <div class="vignette-methodology">
             <h4 class="methodology-title" v-html="$t('vignettes.methodology')"></h4>
             <div>
-              <p class="methodology"></p>
-              <div class="query-container"></div>
+              <div class="query-container">
+                <p class="methodology" v-html="$t('vignettes.query-subtitle')"></p>
+                <div>
+                  <p class="methodology-query">"cambio climático" | "cambio climatico"</p>&nbsp;
+                  <a href target="_blank">(link)</a>&nbsp; VS
+                  <br />
+
+                  <p
+                    class="methodology-query"
+                  >"crisis climática" | "crisis climatica" | "emergencia climática" | "emergencia climatica"</p>&nbsp;
+                  <a href target="_blank">(link)</a>
+                </div>
+              </div>
             </div>
             <p class="methodology-note"></p>
           </div>
@@ -160,8 +257,6 @@ u {
   margin: 20px auto;
 }
 
-// TODO: again, dupicated CSS due to scoped problems
-
 mark {
   color: $color-neutral-0;
   font-weight: 600;
@@ -171,11 +266,11 @@ mark {
 
   &.item1 {
     color: $color-neutral-1000;
-    background-color: mix($color-highlight-1, white, 85%);
+    background-color: rgba($color: $color-highlight-2, $alpha: 0.5);
   }
   &.item2 {
     color: $color-neutral-1000;
-    background-color: mix($color-highlight-2, white, 85%);
+    background-color: rgba($color: $color-highlight-1, $alpha: 0.8);
   }
 }
 
@@ -190,12 +285,7 @@ mark {
   margin: 0 auto;
   margin-bottom: 2rem;
   max-width: 850px;
-
-<<<<<<< HEAD
-  border: 1px solid $color-neutral-1000;
-=======
   border: 1px double $color-neutral-1000;
->>>>>>> master
 
   .vignette-title {
     color: $color-neutral-1000;
@@ -219,13 +309,20 @@ mark {
   border-top: 2px solid $color-neutral-0;
   padding-top: 1rem;
   opacity: 0.8;
+  text-align: left;
 
   .methodology-title {
     color: $color-neutral-1000;
     font-size: 1rem;
   }
   .query-container {
-    display: inline-block;
+    display: flex;
+
+    a {
+      text-decoration: underline;
+      // font-weight: 800;
+      // color: $color-highlight-1;
+    }
   }
   .methodology {
     display: inline-block;
@@ -237,6 +334,7 @@ mark {
     background-color: $color-neutral-0;
     padding: 0.2rem 0.7rem;
     border: 1px solid $color-neutral-800;
+    margin-bottom: 0.5rem;
   }
   .methodology-note {
     font-size: 0.8rem;
