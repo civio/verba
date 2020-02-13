8786f4bcad0a0c01656ddf800cfdf12bc658b3f5
carmen-tm <carmentorremolina@gmail.com>
2020-02-13 17:15:37 +0100

    Fix charts layouts for civio/verba#58

--- web/src/Vignettes.vue
+++ web/src/Vignettes.vue
@@ -13,9 +13,7 @@
               ultraderecha de otros países.
             </p>
             <div class="chart-annotated" v-html="chart_tema01"></div>
-            <p>
-              La palabra "ultraderecha" empezó a coger fuerza en el Telediario en 2017, cuando se hablaba de su auge en Francia y en Holanda. Y empezó a ganar más peso a partir de mediados de 2018, con la irrupción de Bolsonaro en Brasil o de partidos fascistas en Alemania. De hecho, uno de los dos mayores picos, en septiembre de ese año, se explican por las noticias internacionales sobre esos dos países.
-            </p>
+            <p>La palabra "ultraderecha" empezó a coger fuerza en el Telediario en 2017, cuando se hablaba de su auge en Francia y en Holanda. Y empezó a ganar más peso a partir de mediados de 2018, con la irrupción de Bolsonaro en Brasil o de partidos fascistas en Alemania. De hecho, uno de los dos mayores picos, en septiembre de ese año, se explican por las noticias internacionales sobre esos dos países.</p>
             <p>
               La semana en la que esa palabra fue más popular fue justo la última de campaña electoral justo antes de los comicios del 11-N. En solo cuatro días, se repitió 17 veces entre los dos informativos, el de la mañana y el de la noche. Se referían a Vox. Pero no porque en el Telediario se refirieran a ellos como partido de
               ultraderecha, sino por las apariciones de Pedro Sánchez en actos de campaña, entrevistas o el debate electoral criticando los pactos de PP y Ciudadanos con Vox o pidiendo el voto para frenar, claro, a la
@@ -64,9 +62,7 @@
               Gürtel, con la Audiencia Nacional decidiendo si fijaba medidas cautelares para los acusados y la moción de censura contra Mariano Rajoy en marcha, este caso de
               corrupción fue la estrella de los Telediarios, con su apogeo el día 31, el de la moción que cambió a Rajoy por Sánchez en la presidencia del Gobierno.
             </p>
-            <p>
-              Para el caso de los ERE, esa fecha mágica llegó el 19 de noviembre, con la sentencia que condenó a los expresidentes andaluces Manuel Chaves y José Antonio Griñán, entre otros.
-            </p>
+            <p>Para el caso de los ERE, esa fecha mágica llegó el 19 de noviembre, con la sentencia que condenó a los expresidentes andaluces Manuel Chaves y José Antonio Griñán, entre otros.</p>
           </div>
           <div class="vignette-methodology">
             <h4 class="methodology-title" v-html="$t('vignettes.methodology')"></h4>
@@ -149,14 +145,18 @@
           <h3 class="vignette-title">Rajoy sigue vivo</h3>
           <div class="vignette-copy">
             <p>
-              ¿Quién es el personaje más nombrado del Telediario? Si dios ha aparecido unas mil veces desde 2014, Messi suma más de seis dioses (6.467) y Trump, más de siete (7.603). Pero hay alguien que dobla al astro argentino y al endiosado presidente estadounidense.
+              ¿Quién es el personaje más nombrado del Telediario? Si <strong>dios</strong> ha aparecido unas mil veces desde 2014, <strong>Messi</strong> suma más de seis dioses (6.467) y <strong>Trump</strong>, más de siete (7.603). Pero hay alguien que dobla al astro argentino y al endiosado presidente estadounidense.
               <mark>Mariano Rajoy</mark> ha sido mencionado 13.134 veces. Tiene cierta lógica, porque fue presidente durante casi todo el periodo que abarca Verba (de enero de 2014 a junio de 2018). Pero su peso como expresidente sigue siendo considerable.
             </p>
             <div class="chart-annotated" v-html="chart_tema04"></div>
             <p>Desde que fue sustituido en su escaño por un bolso mientras pasaba ocho horas en un bar cercano al Congreso de los Diputados, donde se celebraba la moción que acabó con su presidencia, ha sido mencionado 628 veces en los Telediarios de la televisión pública. Parece poco, pero no lo es si tenemos en cuenta que estamos hablando de solo 20 meses, hasta el 31 de enero de 2020. Así, de media, se le menta una vez al día, una cifra muy superior a las de los otros expresidentes.</p>
-            <p>A José María Aznar (503) y Felipe González (469) se les hace casito cada tanto en el Telediario y ambos rondan las 500 apariciones en algo más de seis meses. Zapatero, más cercano en el tiempo (el registro empieza tres años después de su adiós), les supera, con 781.</p>
-            <p>Puigdemont, por su parte, aparece 4.436 veces, con tres hitos: el referendum y la posterior fuga en octubre de 2017, el pleno de investidura en enero de 2018 y la detención en Alemania en marzo de ese mismo año.</p>
-            <p>Puigdemont, por su parte, aparece 4.436 veces, con tres hitos: el referendum y la posterior fuga en octubre de 2017, el pleno de investidura en enero de 2018 y la detención en Alemania en marzo de ese mismo año.</p>
+            <p>
+              A
+              <strong>José María Aznar</strong> (503) y
+              <strong>Felipe González</strong> (469) se les hace casito cada tanto en el Telediario y ambos rondan las 500 apariciones en algo más de seis meses. <strong>Zapatero</strong>, más cercano en el tiempo (el registro empieza tres años después de su adiós), les supera, con 781.
+            </p>
+            <p><strong>Puigdemont</strong>, por su parte, aparece 4.436 veces, con tres hitos: el referendum y la posterior fuga en octubre de 2017, el pleno de investidura en enero de 2018 y la detención en Alemania en marzo de ese mismo año.</p>
+            <p>¿Y <strong>Pedro Sánchez</strong>? El actual Presidente del Gobierno empezó a asomarse por la televisión pública a mediados de 2015 y desde entonces lo ha hecho más de 9.000 veces, 3,794 desde que llegó a La Moncloa.</p>
           </div>
           <div class="vignette-methodology">
             <h4 class="methodology-title" v-html="$t('vignettes.methodology')"></h4>
@@ -187,7 +187,7 @@
             <p>
               Cuando a finales del 2019, Greta Thunberg dió un discurso en la Cumbre del Clima de Madrid, dejó claro su enfado y sentenció: "Mientras la
               crisis climática continúa siendo ignorada por quienes tienen que tomar las decisiones, hay personas muriendo a día de hoy". No sabemos qué ocurre con aquellos que tienen que decidir, pero lo que tenemos claro es que la
-              "crisis climática"</mark>en los telediarios de RTVE no empezó a aparecer hasta 2019. Solo dos veces antes se había citado como tal: en 2016, por la ONG Acción contra el Hambre, y en la boca de Greta en 2018. De la propia Greta se ha hablado desde ese mismo momento, en concreto, en 75 ocasiones.
+              "crisis climática" en los telediarios de RTVE no empezó a aparecer hasta 2019. Solo dos veces antes se había citado como tal: en 2016, por la ONG Acción contra el Hambre, y en la boca de Greta en 2018. De la propia Greta se ha hablado desde ese mismo momento, en concreto, en 75 ocasiones.
             </p>
             <div class="chart-annotated" v-html="chart_tema05"></div>
             <p>
@@ -389,7 +389,12 @@ mark {
   }
 }
 
+strong {
+  color: $color-neutral-1000;
+}
+
 .verba-vignettes {
+  margin: 1.5rem 0;
   // background-color: pink;
 }
 
