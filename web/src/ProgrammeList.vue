c1f4b8b3f58b456705a97a01e3fc1601c67200f1
Antonio Hernández <antonio@civio.es>
2020-02-14 20:20:52 +0100

    Not displaying months greater than today date in pagination

--- web/src/ProgrammeList.vue
+++ web/src/ProgrammeList.vue
@@ -4,12 +4,12 @@
       <div id="menu-filter">
         <ul>
           <li v-for="year in years" v-bind:class="{'is-clicked':year.num === currentYear}">
-            <a v-on:click="seeYear(year)" v-bind:data-value="year.num" >{{year.num}}</a>
+            <a v-on:click="seeYear(year.num)" v-bind:data-value="year.num">{{year.num}}</a>
           </li>
         </ul>
         <ul v-if="currentYear != ''">
-          <li v-for="month in months" v-bind:class="{'is-clicked':month.num ===  currentMonth}">
-            <a v-on:click="seeMonth(month)" v-bind:data-value="month.num">{{month.name}}</a>
+          <li v-for="month in months" v-bind:class="{'is-clicked':month.num ===  currentMonth}" v-bind:data-ts="month.ts" v-if="today.getTime() >= month.ts">
+            <a v-on:click="seeMonth(month.num)" v-bind:data-value="month.num">{{month.name}}</a>
           </li>
         </ul>
       </div>
@@ -53,8 +53,8 @@ export default {
       subList: [],
       limit: 70,
       inc: undefined,
+      today: new Date(),
       years: [
-        { num: '2013' },
         { num: '2014' },
         { num: '2015' },
         { num: '2016' },
@@ -64,29 +64,31 @@ export default {
         { num: '2020' }
       ],
       months: [
-        { num: '01', name: 'enero' },
-        { num: '02', name: 'febrero' },
-        { num: '03', name: 'marzo' },
-        { num: '04', name: 'abril' },
-        { num: '05', name: 'mayo' },
-        { num: '06', name: 'junio' },
-        { num: '07', name: 'julio' },
-        { num: '08', name: 'agosto' },
-        { num: '09', name: 'septiembre' },
-        { num: '10', name: 'octubre' },
-        { num: '11', name: 'noviembre' },
-        { num: '12', name: 'diciembre' }
+        { num: '1', name: 'enero', ts:'' },
+        { num: '2', name: 'febrero', ts:''},
+        { num: '3', name: 'marzo', ts:'' },
+        { num: '4', name: 'abril', ts:'' },
+        { num: '5', name: 'mayo', ts:'' },
+        { num: '6', name: 'junio', ts:'' },
+        { num: '7', name: 'julio', ts:'' },
+        { num: '8', name: 'agosto', ts:'' },
+        { num: '9', name: 'septiembre', ts:'' },
+        { num: '10', name: 'octubre', ts:'' },
+        { num: '11', name: 'noviembre', ts:'' },
+        { num: '12', name: 'diciembre', ts:'' }
       ],
       currentYear: '',
-      currentMonth: ''
+      currentMonth: '',
+      limitDate:undefined
     }
   },
   mounted() {
     Vue.verbaAPI('fetchProgrammeList', null, response => {
       this.inc = this.limit
       this.programmeList = response.data
-      this.filterProgramme = response.data
-      this.subList = this.get_sublist(this.programmeList)
+      this.seeYear(this.get_today_year()) 
+      this.seeMonth(this.get_today_month())
+      this.limitDate = this.get_limit_date()
     })
   },
 
@@ -98,11 +100,23 @@ export default {
       }
     },
 
+    get_today_year(){
+      return String(this.today.getFullYear())
+    },
+
+    get_today_month(){
+      return String(this.today.getMonth()+1)
+    },
+
+    get_limit_date(){
+      //return new Date(this.)
+    },
+
     get_year_month() {
       return this.programmeList.filter(
         d =>
           this.currentYear === d.date.split('-')[0] &&
-          this.currentMonth === d['date'].split('-')[1]
+          this.currentMonth === String(parseInt(d['date'].split('-')[1]))
       )
     },
 
@@ -118,25 +132,26 @@ export default {
 
     seeYear(year) {
       this.inc = this.limit
-      if (this.currentYear !== year.num) {
-        this.currentYear = year.num
-        if (this.currentMonth !== '') {
-          this.filterProgramme = this.get_year_month()
-        } else {
-          this.filterProgramme = this.get_year()
-        }
+      if (this.currentYear !== year) {
+        this.currentMonth = ''
+        this.currentYear = year
+        this.filterProgramme = this.get_year()
+        this.months.forEach(function(d){
+          d.ts = new Date(this.currentYear + '/' + d.num+'/2').getTime()
+        }.bind(this))
       } else {
         this.currentYear = ''
         this.currentMonth = ''
         this.filterProgramme = this.programmeList
       }
+
       this.subList = this.get_sublist(this.filterProgramme)
     },
 
     seeMonth(month) {
       this.inc = this.limit
-      if (this.currentMonth !== month.num) {
-        this.currentMonth = month.num
+      if (this.currentMonth !== month) {
+        this.currentMonth = month
         this.filterProgramme = this.get_year_month()
       } else {
         this.currentMonth = ''
