ddf786d46038085ccea79f61fb2cddcd890b525a
carmen-tm <carmentorremolina@gmail.com>
2020-02-28 14:14:40 +0100

    Change the deep-link to the currentResult instead of contextResult as a better solution for civio/verba#90

--- web/src/components/ResultContext.vue
+++ web/src/components/ResultContext.vue
@@ -91,7 +91,8 @@ export default {
     onGoToFullTranscriptionBtnClick() {
       const programme_id = this.currentResult.programme.id
       const context_start_time = this.resultContext[0].time_start
-      const URL = `/programmes/${programme_id}#${context_start_time}`
+      const current_start_time = this.currentResult.time_start
+      const URL = `/programmes/${programme_id}#${current_start_time}`
       window.open(URL, '_blank')
     }
   }
