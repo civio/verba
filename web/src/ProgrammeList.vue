9192061eae6c41ca6275079b0f08f49748dba67c
Antonio Hernández <antonio@civio.es>
2022-01-04 14:21:26 +0100

    Make text of the menu unselectable

--- web/src/ProgrammeList.vue
+++ web/src/ProgrammeList.vue
@@ -250,6 +250,13 @@ u {
 
 #menu-filter ul li a {
   cursor: pointer;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+  supported by Chrome, Edge, Opera and Firefox */
 }
 
 #menu-filter ul li.is-clicked {
